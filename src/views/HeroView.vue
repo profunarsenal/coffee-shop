e6573db1c494c439b6ac1ac90e171bdac9946bbc
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-06-01 10:40:59 +0400

    find errors

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -93,19 +93,19 @@ export default {
         {
           id: 0,
           image: "coffee-1.jpg",
-          title: "Solimo Coffee Beans 2kg",
+          name: "Solimo Coffee Beans 2kg",
           price: 10.73,
         },
         {
           id: 1,
           image: "coffee-2.jpg",
-          title: "Presto Coffee Beans 1kg",
+          name: "Presto Coffee Beans 1kg",
           price: 15.99,
         },
         {
           id: 2,
           image: "coffee-3.jpg",
-          title: "AROMISTICO Coffee 1kg",
+          name: "AROMISTICO Coffee 1kg",
           price: 6.99,
         },
       ],
