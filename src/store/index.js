f949a4cc586ad8218a932d633c1446665949ba72
Dmitry Shiryaev <profunarsenal@gmail.com>
2022-06-03 12:02:42 +0400

    vuex data load

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import links from '@/store/links'
+import bestsellers from '@/store/bestsellers'
+import coffees from '@/store/coffees'
+import goods from '@/store/goods'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    links
+    links,
+    bestsellers,
+    coffees,
+    goods
   }
 })
 
